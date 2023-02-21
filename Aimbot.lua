ee765024f1f0938b9d81294c09640ce0b66c25bf
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-02-21 00:15:56 -0500

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -1,13 +1,3 @@
---// Made By YellowGreg \\--
-game:GetService("StarterGui"):SetCore("SendNotification",{
-  Title = "AdvanceFalling Team",
-    Text = "YellowGreg#7993 & MMSVon Bitches",
-    Icon = "rbxthumb://type=Asset&id=9863339777&w=150&h=150",
-    Duration = 8,
-  }
-
-
---// i think this was the local stuff but i dont give a fuck \\--
 local Camera = workspace.CurrentCamera
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
@@ -15,36 +5,18 @@ local UserInputService = game:GetService("UserInputService")
 local TweenService = game:GetService("TweenService")
 local LocalPlayer = Players.LocalPlayer
 local Holding = false
---// THE FUCKING SETTING SHIT \\--
-  -- Main Setting
+-- Main Setting
 _G.AimbotEnabled = true
-_G.TeamCheck = true --// If set to true then the script would only lock your aim at enemy team members.
+_G.TeamCheck = true --// If set to true then the script would only lock on your aim at enemy team members.
 _G.AimPart = "Head" --// Where the aimbot script would lock on at.
-_G.Sensitivity = 0 --// How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
-  -- FOVCircle Setting 
-_G.CircleSides = 64 --// How many sides the FOV circle would have.
-_G.CircleColor = Color3.fromRGB(255, 255, 255) --// (RGB) Color that the FOV circle would appear.
-_G.CircleTransparency = 0.8 --// Transparency of the circle(its like invisible).
-_G.CircleRadius = 80 --// The radius of the circle / FOV.
-_G.CircleFilled = false --// Fill The Circle or Not.
-_G.CircleVisible = true --// Make The Circle Go Away or Stay.
-_G.CircleThickness = 0 --// The thickness of the circle.
+_G.Sensitivity = 0.5 --// How many seconds it takes for the aimbot script to officially lock onto the target's aimpart.
 
---// The Fucking FOV Shit \\--
-local CircleBitch = Drawing.new("Circle")
-CircleBitch.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
-CircleBitch.Radius = _G.CircleRadius
-CircleBitch.Filled = _G.CircleFilled
-CircleBitch.Color = _G.CircleColor
-CircleBitch.Visible = _G.CircleVisible
-CircleBitch.Radius = _G.CircleRadius
-CircleBitch.Transparency = _G.CircleTransparency
-CircleBitch.NumSides = _G.CircleSides
-CircleBitch.Thickness = _G.CircleThickness
---// Aimbot Function Here Bitch \\--
 local function GetClosestPlayer()
-	local MaximumDistance = _G.CircleRadius
+	local MaximumDistance = math.huge
 	local Target = nil
+	coroutine.wrap(function()
+			wait(20); MaximumDistance = math.huge 
+		end)()
 	for _, v in next, Players:GetPlayers() do
 		if v.Name ~= LocalPlayer.Name then
 			if _G.TeamCheck == true then
@@ -56,19 +28,21 @@ local function GetClosestPlayer()
 								local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
 								if VectorDistance < MaximumDistance then
 									Target = v
+									MaximumDistance = VectorDistance
 								end
 							end
 						end
 					end
 				end
-      else
-        if v.Character ~= nil then
+			else
+				if v.Character ~= nil then
 					if v.Character:FindFirstChild("HumanoidRootPart") ~= nil then
 						if v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("Humanoid").Health ~= 0 then
 							local ScreenPoint = Camera:WorldToScreenPoint(v.Character:WaitForChild("HumanoidRootPart", math.huge).Position)
 							local VectorDistance = (Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y) - Vector2.new(ScreenPoint.X, ScreenPoint.Y)).Magnitude
 							if VectorDistance < MaximumDistance then
 								Target = v
+               							MaximumDistance = VectorDistance
 							end
 						end
 					end
@@ -88,20 +62,8 @@ UserInputService.InputEnded:Connect(function(Input)
         Holding = false
     end
 end)
---// Fov Shit Here i guess \\--
 RunService.RenderStepped:Connect(function()
-    CircleBitch.Position = Vector2.new(UserInputService:GetMouseLocation().X, UserInputService:GetMouseLocation().Y)
-    CircleBitch.Radius = _G.CircleRadius
-    CircleBitch.Filled = _G.CircleFilled
-    CircleBitch.Color = _G.CircleColor
-    CircleBitch.Visible = _G.CircleVisible
-    CircleBitch.Radius = _G.CircleRadius
-    CircleBitch.Transparency = _G.CircleTransparency
-    CircleBitch.NumSides = _G.CircleSides
-    CircleBitch.Thickness = _G.CircleThickness
-
     if Holding == true and _G.AimbotEnabled == true then
         TweenService:Create(Camera, TweenInfo.new(_G.Sensitivity, Enum.EasingStyle.Sine, Enum.EasingDirection.Out), {CFrame = CFrame.new(Camera.CFrame.Position, GetClosestPlayer().Character[_G.AimPart].Position)}):Play()
     end
 end)
-print("Made by YellowGreg, and MMSVon Gay Shit")
